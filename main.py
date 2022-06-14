69507c7ba4969791c0641c9467beeeb21efd8692
koxan <koxanovskij86@mail.ru>
2022-06-14 19:33:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Методы списков - append / remove / insert / sort / count
+test_lis = []
+test_lis.append('first method')
+test_lis.append('second method')
+print(test_lis)
+
+test_lis.remove('first method')
+print(test_lis)
+
+test_lis.insert(1, 'first method')
+print(test_lis)
+
+test_lis.sort()
+print(test_lis)
+
+print(test_lis.count('second method'))
+
+# Методы строк upper / lower / //
+test_string = 'Hellow new world'
+print(test_string)
+print(test_string.upper())
+print(test_string.lower())
+
+
+# Методы множества
+
+# Меоды словарей  get / keys / items / values /
+
+test_dict = {'name': 'Vadim', 'second name': 'Ivanov'}
+print(test_dict.get('name'))
+print(test_dict.keys())
+print((test_dict.items()))
+print(test_dict.values())
+#
